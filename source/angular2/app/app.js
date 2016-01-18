5b8d749dfed35a5788d2205103e966a436c4f6ed
John Urberg <jurberg@gmail.com>
2016-01-17 20:10:40 -0600

    work in progress

--- source/angular2/app/app.js
+++ source/angular2/app/app.js
@@ -1,53 +1,36 @@
-System.register(['angular2/platform/browser', 'angular2/core'], function(exports_1) {
-    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-        return c > 3 && r && Object.defineProperty(target, key, r), r;
-    };
-    var __metadata = (this && this.__metadata) || function (k, v) {
-        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-    };
-    var browser_1, core_1;
-    var PhoneService, AppComponent;
-    return {
-        setters:[
-            function (browser_1_1) {
-                browser_1 = browser_1_1;
-            },
-            function (core_1_1) {
-                core_1 = core_1_1;
-            }],
-        execute: function() {
-            PhoneService = (function () {
-                function PhoneService() {
-                    this.phones = [
-                        { 'name': 'Nexus S',
-                            'snippet': 'Fast just got faster with Nexus S.' },
-                        { 'name': 'Motorola XOOM™ with Wi-Fi',
-                            'snippet': 'The Next, Next Generation tablet.' },
-                        { 'name': 'MOTOROLA XOOM™',
-                            'snippet': 'The Next, Next Generation tablet.' }
-                    ];
-                }
-                return PhoneService;
-            })();
-            AppComponent = (function () {
-                function AppComponent(phoneService) {
-                    this.phones = phoneService.phones;
-                }
-                AppComponent = __decorate([
-                    core_1.Component({
-                        selector: 'my-app',
-                        templateUrl: 'app/app.html'
-                    }), 
-                    __metadata('design:paramtypes', [PhoneService])
-                ], AppComponent);
-                return AppComponent;
-            })();
-            exports_1("AppComponent", AppComponent);
-            browser_1.bootstrap(AppComponent, [PhoneService]);
-        }
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var browser_1 = require('angular2/platform/browser');
+var core_1 = require('angular2/core');
+var PhoneService = (function () {
+    function PhoneService() {
+        this.phones = [
+            { 'name': 'Nexus S',
+                'snippet': 'Fast just got faster with Nexus S.' },
+            { 'name': 'Motorola XOOM™ with Wi-Fi',
+                'snippet': 'The Next, Next Generation tablet.' },
+            { 'name': 'MOTOROLA XOOM™',
+                'snippet': 'The Next, Next Generation tablet.' }
+        ];
     }
-});
+    return PhoneService;
+})();
+var AppComponent = (function () {
+    function AppComponent(phoneService) {
+        this.phones = phoneService.phones;
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            templateUrl: 'app/app.html'
+        })
+    ], AppComponent);
+    return AppComponent;
+})();
+exports.AppComponent = AppComponent;
+browser_1.bootstrap(AppComponent, [PhoneService]);
 //# sourceMappingURL=app.js.map
